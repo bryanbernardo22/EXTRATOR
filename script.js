8a104510c0494f3f71318fb8058989bfa42006a5
bryanbernardo22 <bryan.bernardo.santos@escola.pr.gov.br>
2025-08-28 16:56:28 -0300

    bryan

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-cons botaoMostraPalavras = document.querySelector();co
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector('#entrada-de-texto'). value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = processatexto (texto);
+
+    campoResultado.textContent = palavras.join("- ");
+}
+
+function processatexto(texto){
+    let palavras = texto.split(/[^a-Zn-Z]+/);
+    return palavras;
+}
